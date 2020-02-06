57fde37a93a777c9cc2a73b726b761305c181f91
Evgeniy <evgeniyka08@i.ua>
2020-02-06 12:26:59 +0200

    HW-3

---
+++ HW-3/task-04.js
@@ -0,0 +1,26 @@
+"use strict";
+const countTotalSalary = function(employees) {
+  let totalSalary = 0;
+  const valueOfEmployees = Object.values(employees);
+  for (const value of valueOfEmployees) {
+    totalSalary = totalSalary + value;
+  }
+  return totalSalary;
+};
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150
+  })
+); // 400
